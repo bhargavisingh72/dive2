8653bdd9bbca14e2464ac01da3ba7ff0e19d87c3
bhargavisingh72 <bhargavisingh72@gmail.com>
2017-07-04 15:39:42 +0530

    Jdbc Template

--- src/main/java/com/dao/UserDao.java
+++ src/main/java/com/dao/UserDao.java
@@ -1,61 +1,64 @@
 package com.dao;
 
 import java.util.List;
-import java.util.logging.Logger;
 
+import org.apache.log4j.Logger;
 import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.JdbcTemplate;
 
 import com.connectionutil.ConnectionUtil;
 import com.model.UserModel;
 
+
 public class UserDao {
-	private static final Logger LOGGER = Logger.getLogger( UserDao.class.getName() );
-
-	private JdbcTemplate jdbcTemplate= ConnectionUtil.getJdbcTemplate();
+	
+	final static Logger logger = Logger.getLogger(UserDao.class);
 	
 	
-	public List<UserModel> findAll(){
+	public void insertUser(UserModel user) throws Exception {
+        
+		
+		JdbcTemplate jdbcTemplate = ConnectionUtil.getJdbcTemplate();
+ 
+        
+        String sql = "insert into user (id , name, username , password ,mobile_no , email_ID , active, user_roleId) values (?,?,?,?,?,?,?,?)";
+        
+        // 3. Set the input and Query execute
+        int rows = jdbcTemplate.update(sql, user.getId(), user.getName(), user.getUsername(), user.getPassword(), user.getMobileno(), user.getEmailid(), user.getActive(), user.getRole());
+        logger.info("No of rows inserted: " + rows);	
+    }
+	
 
-		String sql = "SELECT * FROM USER";
-
-		List<UserModel> usermodel  = jdbcTemplate.query(sql,
-				new BeanPropertyRowMapper(UserModel.class));
-
-		return usermodel;
+	public void loginUser(UserModel user) throws Exception {
+		
+		JdbcTemplate jdbcTemplate = ConnectionUtil.getJdbcTemplate();
+		
+       
+        String sql = "select * from user where (userName= ?) and (password = ?)";
+		
+        
+        List<UserModel>  users = jdbcTemplate.query(sql, new BeanPropertyRowMapper<UserModel>(UserModel.class), user.getUsername(), user.getPassword());
+        logger.info("Login Success ");
+        logger.info("User Details : " + users);
 	}
 	
-	/*public int findTotalUser(){
-
-		String sql = "SELECT COUNT(*) FROM USER";
-
-		int total = jdbcTemplate.queryForInt(sql);
-
-		return total;
-	}*/
 	
-	public void saveOrUpdate(UserModel usermodel) {
-	    if (usermodel.getId() < 0) {
-	        // update
-	        String sql = "UPDATE USER SET name=?, username=?, password=?, "
-	                    + " WHERE id=?";
-	        jdbcTemplate.update(sql, usermodel.getName(), usermodel.getUsername(),
-	        		usermodel.getPassword(),usermodel.getId());
-	    } else {
-	        // insert
-	        String sql = "INSERT INTO USER (id,name,username,password,mobileno,emailid,active,role)"
-	                    + " VALUES (?, ?, ?, ?, ?, ?, ?, ?)";
-	        jdbcTemplate.update(sql, usermodel.getId(), usermodel.getName(),
-	        		usermodel.getUsername(),usermodel.getPassword(),usermodel.getMobileno(),
-	        		usermodel.getEmailid(),usermodel.getActive(),usermodel.getRole());
-	    }
-	 
+	public void updatePassword(UserModel user, String newPassword) throws Exception {
+		
+		JdbcTemplate jdbcTemplate = ConnectionUtil.getJdbcTemplate();
+		
+        
+		String sql = "update user set password = ? where ((username = ?) and (active = 'A')) and password=?";
+		
+		
+		int rows = jdbcTemplate.update(sql, newPassword, user.getUsername(), user.getPassword());
+		System.out.println("No of rows inserted: " + rows);
+		if (rows == 1) 	
+			logger.info("Password Reset Sucess");
+		else if (rows == 0) 
+			logger.info("Please Login to reset");
+		else 
+			logger.info("Error Reset");
 	}
-	
-	public void delete(int id) {
-	    String sql = "DELETE FROM USER WHERE id=?";
-	    jdbcTemplate.update(sql, id);
-	    
-	    LOGGER.info("Successfully deleted");
-	}
+
 }
